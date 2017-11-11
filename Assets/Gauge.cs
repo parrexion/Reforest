c73fd5082f6249bd28350e9667b931eee55c5982
OscarLundberg <info@oscarlundberg.com>
2017-11-11 14:18:10 +0100

    Gui
    Very wow

---
+++ Assets/Gauge.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Gauge : MonoBehaviour {
+
+	public float someValue;
+	public float maxValue;
+	public Image bar;
+	public float width = 250;
+	public Text t;
+	
+	private float initialWidth;
+
+	void Update() 
+	{
+
+		Visualize();
+	}
+
+	void Visualize() 
+	{
+		float percent = someValue/maxValue;
+		bar.rectTransform.sizeDelta = new Vector2(percent * width, bar.rectTransform.sizeDelta.y);	
+
+		t.text = someValue + " / " + maxValue;
+
+	}
+
+}
