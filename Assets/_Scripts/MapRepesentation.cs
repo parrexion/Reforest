34df0fd392224de386299162124676660b287c24
Parrexion <schack-93@hotmail.com>
2017-11-11 13:31:50 +0100

    Created some basic map representation

--- Assets/_Scripts/MapRepesentation.cs
+++ Assets/_Scripts/MapRepesentation.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class MapRepesentation : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public List<MapTile> map;
+	public Vector2 size;
+	public Vector2 tileSize;
+
+
+	public MapTile getTile(Vector2 position) {
+		return map[convertToIndex(position)];
 	}
+
+	public bool IsWalkable(Vector2 position){
+		return true;
+	}
+
+	public Vector3 CalculatePositionFromCoordinate(Vector2 position){
+		return new Vector3(position.x*tileSize.x, position.y*tileSize.y,0);
+	}
+
 	
-	// Update is called once per frame
-	void Update () {
-		
+	private int convertToIndex(Vector2 position){
+		return (int)position.y*(int)size.x + (int)position.x;
 	}
 }
