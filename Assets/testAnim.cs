1c129f282a2ac25980044dc7c8c336048f0a391b
norrox <christoffer_karlsson@live.se>
2017-11-12 14:41:34 +0100

    Hand

---
+++ Assets/testAnim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testAnim : MonoBehaviour {
+	public Animation animation;
+
+	// Use this for initialization
+	void Start () {
+		animation = GetComponent<Animation>();
+		animation.Play(animation.clip.name);
+        //yield return new WaitForSeconds(animation.clip.length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
