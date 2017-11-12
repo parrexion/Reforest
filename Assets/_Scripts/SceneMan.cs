847bd7f46302ecfbb93a5afa17630c5c3d163220
Parrexion <schack-93@hotmail.com>
2017-11-12 15:31:23 +0100

    Mnyer och timer

--- Assets/_Scripts/SceneMan.cs
+++ Assets/_Scripts/SceneMan.cs
@@ -5,7 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class SceneMan {
 
-	public static void GameOver() {
+	public static void MainMenu() {
+		SceneManager.LoadScene(0);
+	}
+
+	public static void NewGame() {
+		SceneManager.LoadScene(1);
+	}
+
+	public static void IntroScene() {
 		SceneManager.LoadScene(2);
+	}
+	
+	public static void GameOver() {
+		SceneManager.LoadScene(3);
+	}
+
+	public static void QuitGame() {
+		
 	}
 }
---
+++ Assets/_Scripts/ScoreTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTimer : MonoBehaviour {
+
+	public Text scoreLabel;
+	public float dayLength = 20;
+	private float currentScore;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(updateScore());
+	}
+
+	IEnumerator updateScore() {
+		currentScore++;
+		scoreLabel.text = "Current day: " + (int)(currentScore);
+		yield return new WaitForSeconds(dayLength);
+	}
+}
